df5f702b260b0f4b23b62d2b96b64fbc886c1953
Temo Kurtsikidze <temokurtsikidze@Temos-MacBook-Air.local>
2024-06-30 01:52:00 +0400

    first page finished

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 
 const burger = document.getElementById("burger");
 const nav = document.getElementById("navigation");
+const tbcLogo = document.getElementById("tbc-logo");
 
 const burgerClickHandler = () => {
   burger.classList.toggle("is-active");
   nav.classList.toggle("active");
 };
 
+const tbcLogoClickHandler = () => {
+  window.open("https://www.tbceducation.ge", "_blank");
+};
+
 burger.addEventListener("click", burgerClickHandler);
+tbcLogo.addEventListener("click", tbcLogoClickHandler);
