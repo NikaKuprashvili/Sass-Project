98e74c5077dc58e2af61b44f0648a0d12fb8da37
Temo Kurtsikidze <temokurtsikidze@Temos-MacBook-Air.local>
2024-06-30 21:31:04 +0400

    project finished

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+const scrollSlider = () => {
+  const slideButtons = document.querySelectorAll(".slide-button");
+  const imageList = document.querySelector(".slide-imgs");
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+  slideButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const direction = btn.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = imageList.clientWidth * direction;
+      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+      console.log(scrollAmount);
+    });
+
+    const handleSlideBtns = () => {
+      slideButtons[0].style.display =
+        imageList.scrollLeft <= 0 ? "none" : "block";
+      slideButtons[1].style.display =
+        imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    };
+
+    imageList.addEventListener("scroll", handleSlideBtns);
+  });
+};
+
+window.addEventListener("DOMContentLoaded", scrollSlider);
